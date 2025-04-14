c93c4e9a7f09caa51a757ece784c7f7405deb134
Gustavo <gustavo.gomes@appelsoft.com.br>
2025-04-14 09:36:12 -0300

    Add project files.

---
+++ ScreenSound.Shared.Dados/Modelos/PessoaComAcesso.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Shared.Dados.Modelos
+{
+    //classe definida no aspnetcore Identity
+    public class PessoaComAcesso : IdentityUser<int>
+    {
+
+    }
+}
